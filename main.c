809c82e4274f0563b4d2754903a6aa0b8cf23c04
assslove <houbin-12@163.com>
2014-11-22 20:27:37 +0800

    test

---
+++ main.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  test 
+ *
+ *        Version:  1.0
+ *        Created:  11/22/2014 08:26:30 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:	xiaohou, houbin-12@163.com
+ *   Organization:  XiaoHou, Inc. ShangHai CN. All rights reserved.
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	printf("hello, world");
+	return 0;
+}
