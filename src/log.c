667973697363cee12ad5b0f3798c6869c240ab9f
USER <houbin@ubuntu.ubuntu-domain>
2014-11-24 21:46:05 +0800

    add log.*

---
+++ src/log.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  log.c
+ *
+ *    Description:  log System
+ *
+ *        Version:  1.0
+ *        Created:  2014年11月24日 21时43分55秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:	xiaohou , houbin-12@163.com
+ *   Organization:  XiaoHou, Inc. ShangHai CN. All rights reserved.
+ *
+ * =====================================================================================
+ */
+
+
