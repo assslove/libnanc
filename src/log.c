dfa37a8a1141ecb05a1c7ada9f675ceff3b8dc17
assslove <houbin-12@163.com>
2014-11-24 22:23:47 +0800

    simple log

--- src/log.c
+++ src/log.c
@@ -16,4 +16,24 @@
  * =====================================================================================
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdarg.h>
 
+#include "log.h"
+
+void slog(int llv, uint32_t key, const char* fmt, ...)
+{
+	time_t now = time(NULL);
+	struct tm tm;
+	localtime_r(&now, &tm);
+
+	va_list ap;
+	va_start(ap, fmt);
+	fprintf(stdout, "[%02u:%02u:%02u] %u ", tm.tm_hour, tm.tm_min, tm.tm_sec, key);
+	vfprintf(stdout, fmt, ap);
+	fprintf(stdout, "\n");
+
+	va_end(ap);
+}
