e44f6f6edf28a4d3f239906550f6899ca4d1052e
binhou <binhou@mmoserver.txgroup.com>
2014-11-28 20:22:57 +0800

    add log.*

---
+++ src/log.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  log.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  11/28/2014 08:22:41 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:	genry , houbin-12@163.com
+ *   Organization:  TaoMee, Inc. ShangHai CN. All rights reserved.
+ *
+ * =====================================================================================
+ */
+
+
