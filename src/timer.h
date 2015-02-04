6c8e80a6f605421b84d1b62044f56be91bc090b1
houbin <houbin-12@163.com>
2015-02-04 10:48:07 +0800

    add some

--- src/timer.h
+++ src/timer.h
@@ -51,12 +51,15 @@ inline void timer_handle()
 	}
 }
 
-
+/* @brief 定时器初始化
+ */
 inline void timer_init()
 {
 	min_heap_ctor(&g_base_heap);
 }
 
+/* @brief 定时器销毁
+ */
 inline void timer_fini()
 {
 	min_heap_dtor(&g_base_heap);
@@ -74,16 +77,22 @@ inline int add_timer_event(int timerid_, time_t expire_, timer_callback_t func_,
 	return min_heap_push(&g_base_heap, item);
 }
 
+/* @brief 增加定时器
+ */
 #define ADD_TIMER_EVENT(timerid_, expire_, func_, owner_, data_) \
 	if (add_timer_event(timerid_, expire_, func_, owner_, data_)) { \
 		ERROR(0, "add timer event error [id=%u]", timerid_);\
 		return -1;\
 	}
 
+/* @brief 修改定时器
+ */
 #define MODIFY_TIMER_EVENT(item_, expire_) \
 	item_->expire = expire_; \
 	min_heap_shift_down_(&g_base_heap, item_->min_heap_idx, item_);
 
+/* @brief 移除定时器
+ */
 #define REMOVE_TIMER_EVENT(item_) \
 	min_heap_pop(&g_base_heap); \
 	free(item_);\
